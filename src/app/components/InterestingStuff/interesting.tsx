47c34785eafb302f300e606bf8731d39a4fbe1e7
Krisi <quanchen2047@gmail.com>
2019-07-22 22:44:02 +0900

    add penguin css samples

---
+++ src/app/components/InterestingStuff/interesting.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import "./penguin.css";
+
+export class Penguin extends React.Component<{}, {}> {
+
+    public render() {
+        return (
+            <div className="penguin">
+                <div className="penguin-bottom">
+                    <div className="right-hand"></div>
+                    <div className="left-hand"></div>
+                    <div className="right-feet"></div>
+                    <div className="left-feet"></div>
+                </div>
+                <div className="penguin-top">
+                    <div className="right-cheek"></div>
+                    <div className="left-cheek"></div>
+                    <div className="belly"></div>
+                    <div className="right-eye">
+                        <div className="sparkle"></div>
+                    </div>
+                    <div className="left-eye">
+                        <div className="sparkle"></div>
+                    </div>
+                    <div className="blush-right"></div>
+                    <div className="blush-left"></div>
+                    <div className="beak-top"></div>
+                    <div className="beak-bottom"></div>
+                </div>
+            </div>
+        )
+    }
+}
